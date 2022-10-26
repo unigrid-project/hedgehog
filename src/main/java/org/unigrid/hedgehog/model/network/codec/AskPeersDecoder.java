0b4f43cc22e67526956cdea3969f07dc3a531aec
Adam Waldenberg <adam@unigrid.org>
2022-10-26 14:46:40 +0200

    Add peer ask packets and encoder/deocder

---
+++ src/main/java/org/unigrid/hedgehog/model/network/codec/AskPeersDecoder.java
@@ -0,0 +1,40 @@
+/*
+    Unigrid Hedgehog
+    Copyright © 2021-2022 The Unigrid Foundation, UGD Software AB
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License and the addendum for more details.
+
+    You should have received an addended copy of the GNU Affero General Public License with this program.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+ */
+
+package org.unigrid.hedgehog.model.network.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+import java.util.List;
+import org.unigrid.hedgehog.model.network.codec.api.PacketDecoder;
+import org.unigrid.hedgehog.model.network.packet.AskPeers;
+
+public class AskPeersDecoder extends ReplayingDecoder<AskPeers> implements PacketDecoder<AskPeers> {
+	/*
+	    Packet format:
+	    0..............................................................63
+	    [    amount    ][                  reserved                    ]
+	*/
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+		final AskPeers askPeers = new AskPeers();
+
+		askPeers.setAmount(in.readShort());
+		in.skipBytes(6 /* 48 bits */);
+		out.add(askPeers);
+	}
+}
