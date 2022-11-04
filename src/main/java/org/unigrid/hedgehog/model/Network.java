12942bdf792a9a0ced36d74be19398255a393e16
Adam Waldenberg <adam@unigrid.org>
2022-11-04 14:43:52 +0100

    Increase max packet size from 8MB to 256MB

--- src/main/java/org/unigrid/hedgehog/model/Network.java
+++ src/main/java/org/unigrid/hedgehog/model/Network.java
@@ -29,7 +29,8 @@ public class Network {
 	};
 
 	public static final int COMMUNICATION_THREADS = 16;
-	public static final int MAX_DATA_SIZE = 1024 * 1024 * 8;
+	public static final int MAX_DATA_SIZE = 1024 * 1024 * 256; /* 256 MB */
+
 	public static final int MAX_STREAMS = 64;
 
 	public static final String[] KEYS = {
