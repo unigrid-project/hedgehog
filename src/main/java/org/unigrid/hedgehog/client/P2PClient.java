75b398a01e97f5499ef351b3a8688f3c8ea3e2c4
Adam Waldenberg <adam@unigrid.org>
2022-11-18 03:48:10 +0100

    Handle shutdowns more gracefully in P2PClient.close()

--- src/main/java/org/unigrid/hedgehog/client/P2PClient.java
+++ src/main/java/org/unigrid/hedgehog/client/P2PClient.java
@@ -109,8 +109,14 @@ public class P2PClient {
 
 	@SneakyThrows
 	public void close() {
-		quicStreamChannel.disconnect().sync();
-		group.shutdownGracefully();
+		if (!quicStreamChannel.isShutdown()) {
+			quicStreamChannel.shutdown().sync();
+		}
+
+		if (!group.isShuttingDown()) {
+			group.shutdownGracefully().sync();
+		}
+	}
 
 	public static void send(Packet packet, Node node, Optional<BiConsumer<Node, Future>> consumer) {
 		if (node.getChannel().isPresent()) {
