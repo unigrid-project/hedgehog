3f3a56a8c57f84e74092371080048e944f61ca7b
Adam Waldenberg <adam@unigrid.org>
2023-01-13 08:30:26 +0100

    Add initial Unzipper class using Apache Compress to unarchive ZIP files

---
+++ native-image/src/main/java/org/unigrid/hedgehog/nativeimage/Unzipper.java
@@ -0,0 +1,51 @@
+/*
+    Unigrid Hedgehog
+    Copyright © 2021-2023 The Unigrid Foundation, UGD Software AB
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License and the addendum for more details.
+
+    You should have received an addended copy of the GNU Affero General Public License with this program.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+ */
+
+package org.unigrid.hedgehog.nativeimage;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+
+public class Unzipper {
+	private File create(File destinationDir, ZipEntry zipEntry) throws IOException {
+		File destFile = new File(destinationDir, zipEntry.getName());
+
+		String destDirPath = destinationDir.getCanonicalPath();
+		String destFilePath = destFile.getCanonicalPath();
+
+		if (!destFilePath.startsWith(destDirPath + File.separator)) {
+			throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+		}
+
+		return destFile;
+	}
+
+	public static void unzip(byte[] data, Path destination) throws IOException {
+
+		try (ZipArchiveInputStream archive = new ZipArchiveInputStream(new ByteArrayInputStream(data))) {
+			ZipArchiveEntry entry;
+
+			while ((entry = archive.getNextZipEntry()) != null) {
+				//Files.createDirectories(, fas)
+			}
+		}
+	}
+}
