c39e8bd8b27832c000ec3c948f7bcda4a6ac8a88
Torekhan Abayev <torekhan@unigrid.org>
2023-01-24 12:16:50 +0100

    Add adapter for xml models

---
+++ src/main/java/org/unigrid/hedgehog/model/s3/entity/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/package-info.java to edit this template
+ */
+@XmlSchema(namespace = "http://s3.amazonaws.com/doc/2006-03-01", elementFormDefault = XmlNsForm.QUALIFIED)
+package org.unigrid.hedgehog.model.s3.entity;
+
+import jakarta.xml.bind.annotation.XmlNsForm;
+import jakarta.xml.bind.annotation.XmlSchema;
