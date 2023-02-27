40fa30996373c55300c27ff990381f08d37b5d8d
Adam Waldenberg <adam@unigrid.org>
2023-02-27 06:39:35 +0100

    Add requirement on jdk.crypto.ec module
    Without this, Jersey will not work properly when running via the native
    image, with SSL quietly failing.

--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -56,6 +56,7 @@ module org.unigrid.hedgehog {
 	requires net.harawata.appdirs;
 	requires org.graalvm.sdk;
 	requires org.unigrid.hedgehog.common;
+	requires jdk.crypto.ec;
 
 	opens org.unigrid.hedgehog.model.s3.entity to jakarta.xml.bind;
 }
