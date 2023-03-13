1afa11ca80ae9ce1989fb56f62cb4fe1bb70891e
Adam Waldenberg <adam@unigrid.org>
2023-03-13 18:05:40 +0100

    Introduce polymorphic JSON mapping help to ChunkData interface
    This helps Jackson determine exactly how to handle references to a
    ChunkData and what types it should consider. To get this to work
    properly, a bump to version 2.13.2 of the jackson-datatype-jsr310
    dependency was needed.

--- application/src/main/java/org/unigrid/hedgehog/model/network/chunk/ChunkData.java
+++ application/src/main/java/org/unigrid/hedgehog/model/network/chunk/ChunkData.java
@@ -19,8 +19,20 @@
 
 package org.unigrid.hedgehog.model.network.chunk;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.io.Serializable;
+import org.unigrid.hedgehog.model.spork.MintStorage;
+import org.unigrid.hedgehog.model.spork.MintSupply;
+import org.unigrid.hedgehog.model.spork.VestingStorage;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({
+	@Type(MintStorage.SporkData.class),
+	@Type(MintSupply.SporkData.class),
+	@Type(VestingStorage.SporkData.class)
+})
 public interface ChunkData extends Serializable {
 	/* Empty on purpose */
 }
