c015a2bae744c01dc5fa9d7cb01d6be9a2bee80c
Adam Waldenberg <adam@unigrid.org>
2023-03-20 05:04:22 +0100

    Update VestingStorageResource and add signature check
    With any luck this completes the format of the follwing endpoints:
    
    @GET /gridspork/vesting-storage
    @GET /gridspork/vesting-storage/<address>
    @PUT /gridspork/vesting-storage/<address>

--- application/src/test/java/org/unigrid/hedgehog/server/rest/VestingStorageResourceTest.java
+++ application/src/test/java/org/unigrid/hedgehog/server/rest/VestingStorageResourceTest.java
@@ -19,17 +19,69 @@
 
 package org.unigrid.hedgehog.server.rest;
 
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
 import lombok.SneakyThrows;
-import net.jqwik.api.Example;
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Provide;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.AlphaChars;
+import net.jqwik.api.constraints.BigRange;
+import net.jqwik.api.constraints.ByteRange;
+import net.jqwik.api.constraints.Scale;
+import net.jqwik.api.constraints.StringLength;
+import net.jqwik.time.api.constraints.DurationRange;
+import net.jqwik.time.api.constraints.InstantRange;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import org.unigrid.hedgehog.client.RestClient;
+import org.unigrid.hedgehog.jqwik.TestFileOutput;
+import org.unigrid.hedgehog.model.crypto.Signature;
+import org.unigrid.hedgehog.model.spork.VestingStorage.SporkData.Vesting;
 
 public class VestingStorageResourceTest extends BaseRestClientTest {
-	@Example
-	@SneakyThrows
-	public void shoulBeAbleToSetVestingStorageSpork() {
-		final RestClient client = new RestClient(server.getRest().getHostName(), server.getRest().getPort(), true);
+	@Provide
+	public Arbitrary<Vesting> provideVesting(@ForAll @BigRange(min = "1", max = "1000000") @Scale(12) BigDecimal amount,
+		@ForAll @ByteRange(min = 1) byte parts, @ForAll @DurationRange(min = "P1D", max = "P1000D") Duration duration,
+		@ForAll @InstantRange() Instant start) {
 
-		client.get("/gridspork");
+		return Arbitraries.of(Vesting.builder().amount(amount).duration(duration)
+			.parts(parts).start(start).build());
+	}
+
+	@SneakyThrows
+	@Property(tries = 50)
+	public void shoulBeAbleToGetVestingStorageSpork(@ForAll("provideSignature") Signature signature,
+		@ForAll("provideVesting") Vesting vesting, @ForAll @AlphaChars @StringLength(36) String address) {
+
+		Status expectedStatusFromPut;
+
+		final RestClient client = new RestClient(server.getRest().getHostName(),
+			server.getRest().getPort(), true
+		);
+
+		if (Status.fromStatusCode(client.get("/gridspork/vesting-storage/"+ address).getStatus()) != Status.OK) {
+			expectedStatusFromPut = Status.OK;
+		} else {
+			expectedStatusFromPut = Status.NO_CONTENT;
+		}
+
+		final Response putResponse = client.putWithHeaders("/gridspork/vesting-storage/" + address, vesting,
+			new MultivaluedHashMap(Map.of("privateKey", signature.getPrivateKey()))
+		);
+
+		assertThat(Status.fromStatusCode(putResponse.getStatus()),
+			equalTo(expectedStatusFromPut)
+		);
+
+		TestFileOutput.outputJson(client.getEntity("/gridspork/vesting-storage/" + address, String.class));
 		client.close();
 	}
 }
