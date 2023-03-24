f0538d6f3ffce9d6451c99ace04459ac95468fe3
Adam Waldenberg <adam@unigrid.org>
2023-03-24 07:17:26 +0100

    Improve tests for mint storage and vesting storage endpoints

--- application/src/test/java/org/unigrid/hedgehog/server/rest/MintStorageResourceTest.java
+++ application/src/test/java/org/unigrid/hedgehog/server/rest/MintStorageResourceTest.java
@@ -23,24 +23,29 @@ import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 import lombok.SneakyThrows;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.ForAll;
+import net.jqwik.api.From;
 import net.jqwik.api.Provide;
 import net.jqwik.api.Property;
+import net.jqwik.api.ShrinkingMode;
 import net.jqwik.api.constraints.AlphaChars;
+import net.jqwik.api.constraints.BigRange;
 import net.jqwik.api.constraints.StringLength;
 import net.jqwik.api.constraints.Positive;
+import net.jqwik.api.constraints.Size;
+import net.jqwik.api.constraints.UniqueElements;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import org.unigrid.hedgehog.client.RestClient;
 import org.unigrid.hedgehog.jqwik.TestFileOutput;
 import org.unigrid.hedgehog.model.Address;
 import org.unigrid.hedgehog.model.crypto.Signature;
+import org.unigrid.hedgehog.model.spork.MintStorage;
 import org.unigrid.hedgehog.model.spork.MintStorage.SporkData.Location;
-import org.unigrid.hedgehog.model.spork.VestingStorage.SporkData.Vesting;
 
 public class MintStorageResourceTest extends BaseRestClientTest {
 	@Provide
@@ -53,31 +58,63 @@ public class MintStorageResourceTest extends BaseRestClientTest {
 	}
 
 	@SneakyThrows
-	@Property(tries = 50)
-	public void shoulBeAbleToGetVestingStorageSpork(@ForAll("provideSignature") Signature signature,
-		@ForAll("provideLocation") Location location, BigDecimal amount) {
+	@Property(tries = 30, shrinking = ShrinkingMode.OFF)
+	public void shoulBeVerifiableInList(@ForAll("provideSignature") Signature signature,
+		@ForAll @UniqueElements @Size(5) List<@From("provideLocation") Location> locations,
+		@ForAll @BigRange(min = "0", max = "1000000") BigDecimal amount) {
 
-		//Status expectedStatusFromPut;
+		final String url = "/gridspork/mint-storage/";
+		final Response response = client.get(url);
+		int originalNumMints = 0;
+		int newMints = 0;
 
-		//final RestClient client = new RestClient(server.getRest().getHostName(),
-		//	server.getRest().getPort(), true
-		//);
+		if (Status.fromStatusCode(response.getStatus()) == Status.OK) {
+			final MintStorage.SporkData data = response.readEntity(MintStorage.class).getData();
+			originalNumMints = data.getMints().size();
+		}
 
-		//if (Status.fromStatusCode(client.get("/gridspork/vesting-storage/"+ address).getStatus()) != Status.OK) {
-		//	expectedStatusFromPut = Status.OK;
-		//} else {
-		//	expectedStatusFromPut = Status.NO_CONTENT;
-		//}
+		for (Location l : locations) {
+			final Response putResponse = client.putWithHeaders(url + l.getAddress().getWif() + "/" + l.getHeight(),
+				amount, new MultivaluedHashMap(Map.of("privateKey", signature.getPrivateKey()))
+			);
 
-		//final Response putResponse = client.putWithHeaders("/gridspork/vesting-storage/" + address, vesting,
-		//	new MultivaluedHashMap(Map.of("privateKey", signature.getPrivateKey()))
-		//);
+			if (Status.fromStatusCode(putResponse.getStatus()) == Status.OK) {
+				newMints++;
+			}
+		}
 
-		//assertThat(Status.fromStatusCode(putResponse.getStatus()),
-		//	equalTo(expectedStatusFromPut)
-		//);
-
-		//TestFileOutput.outputJson(client.getEntity("/gridspork/vesting-storage/" + address, String.class));
-		//client.close();
+		if (newMints > 0) {
+			final MintStorage.SporkData data = client.getEntity(url, MintStorage.class).getData();
+			assertThat(data.getMints().size(), equalTo(originalNumMints + newMints));
+		}
 	}
+
+	@SneakyThrows
+	@Property(tries = 50)
+	public void shoulBeAbleToGetMintStorageSpork(@ForAll("provideSignature") Signature signature,
+		@ForAll("provideLocation") Location location, @ForAll @BigRange(min = "0") BigDecimal amount) {
+
+		final int height = location.getHeight();
+		final String wif = location.getAddress().getWif();
+		final String url = "/gridspork/mint-storage/%s/%d".formatted(wif, height);
+
+		Status expectedStatusFromPut;
+
+		if (Status.fromStatusCode(client.get(url).getStatus()) != Status.OK) {
+			expectedStatusFromPut = Status.OK;
+		} else {
+			expectedStatusFromPut = Status.NO_CONTENT;
+		}
+
+		final Response putResponse = client.putWithHeaders(url, amount,
+			new MultivaluedHashMap(Map.of("privateKey", signature.getPrivateKey()))
+		);
+
+		assertThat(Status.fromStatusCode(putResponse.getStatus()),
+			equalTo(expectedStatusFromPut)
+		);
+
+		TestFileOutput.outputJson(client.getEntity(url, String.class));
+	}
+
 }
