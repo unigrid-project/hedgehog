62b70130afba736c6de587be6993ecafc2ac171b
Adam Waldenberg <adam@unigrid.org>
2023-05-24 16:06:14 +0200

    Lower the number of default communication threads to 8

--- application/src/main/java/org/unigrid/hedgehog/model/Network.java
+++ application/src/main/java/org/unigrid/hedgehog/model/Network.java
@@ -29,7 +29,7 @@ public class Network {
 		"localhost"
 	};
 
-	public static final int COMMUNICATION_THREADS = 16;
+	public static final int COMMUNICATION_THREADS = 8;
 	public static final int MAX_DATA_SIZE = 1024 * 1024 * 256; /* 256 MB */
 
 	public static final int MAX_STREAMS = 64;
