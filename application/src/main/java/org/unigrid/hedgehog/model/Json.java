2c656a41e28c522a117f085544fdcb979b24bdf9
Adam Waldenberg <adam@unigrid.org>
2023-06-12 10:56:16 +0200

    Introduce Json utility model for pretty printing JSON strings
    This also rewrites TestFileOutput.outputJson(...) to use this new class.

---
+++ application/src/main/java/org/unigrid/hedgehog/model/Json.java
@@ -0,0 +1,44 @@
+/*
+    Unigrid Hedgehog
+    Copyright © 2021-2023 Stiftelsen The Unigrid Foundation, UGD Software AB
+
+    Stiftelsen The Unigrid Foundation (org. nr: 802482-2408)
+    UGD Software AB (org. nr: 559339-5824)
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License and the addendum for more details.
+
+    You should have received an addended copy of the GNU Affero General Public License with this program.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+ */
+
+package org.unigrid.hedgehog.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Json {
+	public static <T> String parse(T object) {
+		final ObjectMapper mapper = new ObjectMapper();
+		String json = "Invalid JSON";
+
+		try {
+			if (object instanceof String s) {
+				json = mapper.readTree(s).toPrettyString();
+			} else {
+				json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+			}
+		} catch (JsonProcessingException ex) {
+			log.atError().log("Failed to process JSON for {}", object);
+		}
+
+		return json;
+	}
+}
