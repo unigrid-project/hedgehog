fd0019aa896f07519819fd3b525b20153eecc03c
Fim-84 <marcus.stenberg@gmail.com>
2023-11-13 09:26:38 +0100

    added util class for helper functions

---
+++ application/src/main/java/org/unigrid/hedgehog/model/util/Utils.java
@@ -0,0 +1,58 @@
+/*
+    Unigrid Hedgehog
+    Copyright © 2021-2023 Stiftelsen The Unigrid Foundation, UGD Software AB
+
+    Stiftelsen The Unigrid Foundation (org. nr: 802482-2408)
+    UGD Software AB (org. nr: 559339-5824)
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the
+    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License and the addendum for more details.
+
+    You should have received an addended copy of the GNU Affero General Public License with this program.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+ */
+
+package org.unigrid.hedgehog.model.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Utils {
+
+	public static byte[] hexStringToByteArray(String s) {
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+				+ Character.digit(s.charAt(i + 1), 16));
+		}
+		return data;
+	}
+
+	public static String sha256Hash(String input) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(input.getBytes());
+
+			// Convert the byte array to a hexadecimal string
+			StringBuilder hexString = new StringBuilder();
+			for (byte b : hash) {
+				String hex = Integer.toHexString(0xff & b);
+				if (hex.length() == 1) {
+					hexString.append('0');
+				}
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// Handle the exception
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
